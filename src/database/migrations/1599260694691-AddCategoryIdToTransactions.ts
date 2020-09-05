c70dafcb14a11e3d3e2fb9b4189df191fbed96e5
Izaias Neto <izaiasmneto@gmail.com>
2020-09-04 22:58:09 -0300

    Desafio finalizado!

---
+++ src/database/migrations/1599260694691-AddCategoryIdToTransactions.ts
@@ -0,0 +1,32 @@
+import {
+    MigrationInterface, 
+    QueryRunner, 
+    TableColumn, 
+    TableForeignKey
+} from "typeorm";
+
+export class AddCategoryIdToTransactions1599260694691 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.addColumn('transactions', new TableColumn({
+            name: 'category_id',
+            type: 'uuid',
+            isNullable: true
+        }))
+
+        await queryRunner.createForeignKey('transactions', new TableForeignKey({
+            name: 'TransactionsCategory',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'categories',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE'
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+       await queryRunner.dropForeignKey('transactions', 'TransactionsCategory');
+
+       await queryRunner.dropColumn('transactions', 'category_id');
+
+    }
+}
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm'
+
+import TransactionsRepository from '../repositories/TransactionsRepository'
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository)
+
+    const checkTransactionExist = await transactionRepository.findOne(id)
+
+    if(!checkTransactionExist){
+      throw new AppError('Transaction does not exist')
+    }
+
+    await transactionRepository.remove(checkTransactionExist)
   }
 }
 
